9a5a87fd9ff9c33ad7604ad2e7d5ef1d16a6d38e
Mateusz Stompór <mateusz.stompor@gmail.com>
2018-06-19 18:21:31 +0200

    bugfixes, add another example, which shows how to use data structure with complex types

--- README.md
+++ README.md
@@ -11,21 +11,38 @@ You need to declare a macro which will be used by the BTS to compare elements.
 If you wonder how does the code run I highly recommend reading an article from the link below.
 https://gcc.gnu.org/onlinedocs/cpp/Macros.html
 
+## Usage
+In order to start using the code include single header as it is shown on the example. 
+
 ```c
 
 #include <stdio.h>
 
 #define binary_tree_int_is_greater(a, b) a > b
+#define binary_tree_Point2D_is_greater(a, b) a.x > b.x
+
+typedef struct Point2D {
+    float x;
+    float y;
+} Point2D;
+
 #include "binary_tree.h"
 
 int main(int argc, const char * argv[]) {
-    binary_tree(int) *a;
-    binary_tree_alloc(int, a);
-    binary_tree_insert(int, a, 2);
-    binary_tree_insert(int, a, 1);
-    binary_tree_insert(int, a, 3);
-    binary_tree_release(int, a);
+
+    binary_tree(int) * intTree;
+    binary_tree_alloc(int, intTree);
+    binary_tree_insert(int, intTree, 2);
+    binary_tree_release(int, intTree);
+
+
+    Point2D p = {2.3f, 3.0f};
+    binary_tree(Point2D) * point2DTree;
+    binary_tree_alloc(Point2D, point2DTree);
+    binary_tree_insert(Point2D, point2DTree, p);
+    binary_tree_release(Point2D, point2DTree);
     return 0;
+
 }
 
 ```
--- binary_tree.h
+++ binary_tree.h
@@ -12,99 +12,99 @@
 #include <stdlib.h>
 
 // do not forget about data alignment
-#define bt_node(T)                  \
-struct bt_node_##T {                \
-    struct bt_node_##T * left;      \
-    struct bt_node_##T * right;     \
-    struct bt_node_##T * parent;    \
-    T data;                         \
-}                                   \
+#define bt_node(T)\
+struct bt_node_##T {\
+    struct bt_node_##T * left;\
+    struct bt_node_##T * right;\
+    struct bt_node_##T * parent;\
+    T data;\
+}\
 
-#define bt_node_alloc(T, bt_node_ptr, l, r, p, v) ({    \
-    bt_node_ptr = malloc(sizeof(struct bt_node_##T));   \
-    bt_node_ptr->right = NULL;                          \
-    bt_node_ptr->left = l;                              \
-    bt_node_ptr->parent = p;                            \
-    bt_node_ptr->data = v;                              \
-})                                                      \
+#define bt_node_alloc(T, bt_node_ptr, l, r, p, v) ({\
+    bt_node_ptr = malloc(sizeof(struct bt_node_##T));\
+    bt_node_ptr->right = NULL;\
+    bt_node_ptr->left = l;\
+    bt_node_ptr->parent = p;\
+    bt_node_ptr->data = v;\
+})\
 
-#define bt_node_release(T, bt_node_ptr) ({  \
-    free(bt_node_ptr);                      \
-})                                          \
+#define bt_node_release(T, bt_node_ptr) ({\
+    free(bt_node_ptr);\
+})\
 
-#define binary_tree(T)          \
-struct binary_tree_##T {        \
-    size_t size;                \
-    bt_node(T) * root;          \
-    struct bt_node_##T * min;   \
-    struct bt_node_##T * max;   \
-}                               \
+#define binary_tree(T)\
+struct binary_tree_##T {\
+    size_t size;\
+    bt_node(T) * root;\
+    struct bt_node_##T * min;\
+    struct bt_node_##T * max;\
+}\
 
-#define binary_tree_alloc(T, tree) ({               \
-    tree = malloc(sizeof(struct binary_tree_##T));  \
-    tree->size = 0;                                 \
-    tree->root = NULL;                              \
-    tree->min = NULL;                               \
-    tree->max = NULL;                               \
-})                                                  \
+#define binary_tree_alloc(T, tree) ({\
+    tree = malloc(sizeof(struct binary_tree_##T));\
+    tree->size = 0;\
+    tree->root = NULL;\
+    tree->min = NULL;\
+    tree->max = NULL;\
+})\
 
 // c - current
-#define binary_tree_release(T, tree_ptr) ({                         \
-    struct bt_node_##T * c = tree_ptr->min;                         \
-    struct bt_node_##T * max = tree_ptr->max;                       \
-    while(c != max) {                                               \
-        if(c->parent != NULL && c->parent->left == c) {             \
-            c = c->parent;                                          \
-            bt_node_release(T, c->left);                            \
-            if(c->right != NULL) {                                  \
-                c = c->right;                                       \
-                while(c->left != NULL) {                            \
-                    c = c->left;                                    \
-                }                                                   \
-            }                                                       \
-        } else {                                                    \
-            do {                                                    \
-                c = c->parent;                                      \
-                bt_node_release(T, c->right);                       \
-            } while (c->parent != NULL || c != c->parent->right);   \
-        }                                                           \
-    }                                                               \
-    while(c != NULL) {                                              \
-        bt_node_release(T, c);                                      \
-        c = c->parent;                                              \
-    }                                                               \
-    free(tree_ptr);                                                 \
-})                                                                  \
+#define binary_tree_release(T, tree_ptr) ({\
+    struct bt_node_##T * c = tree_ptr->min;\
+    struct bt_node_##T * max = tree_ptr->max;\
+    while(c != max) {\
+        if(c->parent != NULL && c->parent->left == c) {\
+            c = c->parent;\
+            bt_node_release(T, c->left);\
+            if(c->right != NULL) {\
+                c = c->right;\
+                while(c->left != NULL) {\
+                    c = c->left;\
+                }\
+            }\
+        } else {\
+            do {\
+                c = c->parent;\
+                bt_node_release(T, c->right);\
+            } while (c->parent != NULL || c != c->parent->right);\
+        }\
+    }\
+    while(c != NULL) {\
+        bt_node_release(T, c);\
+        c = c->parent;\
+    }\
+    free(tree_ptr);\
+})\
 
-#define binary_tree_insert(T, tree, value) ({               \
-    if (tree->root == NULL) {                               \
-        binary_tree_insert_to_empty(T, tree, value);        \
-    } else {                                                \
-        binary_tree_insert_to_non_empty(T, tree, value);    \
-    }                                                       \
-})                                                          \
+#define binary_tree_insert(T, tree, value) ({\
+    if (tree->root == NULL) {\
+        binary_tree_insert_to_empty(T, tree, value);\
+    } else {\
+        binary_tree_insert_to_non_empty(T, tree, value);\
+    }\
+})\
 
-#define binary_tree_insert_to_empty(T, tree, value) ({      \
-    bt_node_alloc(T, tree->root, NULL, NULL, NULL, value);  \
-    tree->size += 1;                                        \
-    tree->min = tree->root;                                 \
-    tree->max = tree->root;                                 \
-})                                                          \
+#define binary_tree_insert_to_empty(T, tree, value) ({\
+    bt_node_alloc(T, tree->root, NULL, NULL, NULL, value);\
+    tree->size += 1;\
+    tree->min = tree->root;\
+    tree->max = tree->root;\
+})\
 
 // n - next, c - current, t - temporary value
-#define binary_tree_insert_to_non_empty(T, tree, value) ({                                              \
-    struct bt_node_##T * c = tree->root;                                                                \
-    struct bt_node_##T * n = value > tree->root->data ? tree->root->right : tree->root->left;           \
-    while(n != NULL) {                                                                                  \
-        c = n;                                                                                          \
-        n =  binary_tree_##T##_is_greater(value, n->data) ? n->right : n->left;                         \
-    }                                                                                                   \
-    struct bt_node_##T ** t = binary_tree_##T##_is_greater(value, c->data) ? &(c->right) : &(c->left);  \
-    bt_node_alloc(T, (*t), NULL, NULL, c, value);                                                       \
-    if(value > tree->max->data) {                                                                       \
-        tree->max=(*t);                                                                                 \
-    } else if (value < tree->min->data) {                                                               \
-        tree->min=(*t);                                                                                 \
-    }                                                                                                   \
-    tree->size += 1;                                                                                    \
-})                                                                                                      \
+#define binary_tree_insert_to_non_empty(T, tree, value) ({\
+    struct bt_node_##T * c = tree->root;\
+    struct bt_node_##T * n = binary_tree_##T##_is_greater(value, tree->root->data) ? tree->root->right : tree->root->left;\
+    while(n != NULL) {\
+        c = n;\
+        n = binary_tree_##T##_is_greater(value, n->data) ? n->right : n->left;\
+    }\
+    struct bt_node_##T ** t = binary_tree_##T##_is_greater(value, c->data) ? &(c->right) : &(c->left);\
+    bt_node_alloc(T, (*t), NULL, NULL, c, value);\
+    if(binary_tree_##T##_is_greater(value, tree->max->data)) {\
+        tree->max=(*t);\
+    } else if (binary_tree_##T##_is_greater(tree->min->data, value)) {\
+        tree->min=(*t);\
+    }\
+    tree->size += 1;\
+})\
